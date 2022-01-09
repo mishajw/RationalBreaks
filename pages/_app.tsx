fcd0e113cc775516fdf6e0346ab21f398bff5dcd
Misha Wagner <mishajw@gmail.com>
2022-01-09 15:43:33 +0100

    Initial commit.
    - Work & break modes.
    - Material UI.
    - Styling.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,48 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import PropTypes from "prop-types";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { AppProps } from "next/app";
+import "../styles/globals.css";
+import { red } from "@material-ui/core/colors";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function RationalBreaks({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
-export default MyApp
+
+RationalBreaks.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+});
